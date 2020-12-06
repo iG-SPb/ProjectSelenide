757c2730f5033b57113b0e2fd151c44b4365490d
iG-SPb <ig_9374318@mail.ru>
2020-12-06 13:22:24 +0300

    new metods append

--- src/test/java/testMarket/MarketNewsPageTest.java
+++ src/test/java/testMarket/MarketNewsPageTest.java
@@ -3,11 +3,27 @@ package testMarket;
 import forAllTest.BaseTest;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import projectMarket.MarketNewsPage;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MarketNewsPageTest extends BaseTest {
+
+    WebDriverWait wait; // переменная для waitForSecondWindow
 
     @Feature(value = "Test page - News company")
     @Severity(SeverityLevel.CRITICAL)
@@ -48,4 +64,55 @@ public class MarketNewsPageTest extends BaseTest {
     public void dummy() {
 
     }
+/*
+    @Test
+    public void addDragAndDropTest() {
+        // элемент для DragAndDrop
+        WebElement fieldOne = driver.findElement(By.xpath("xpath One"));
+        // куда DragAndDrop
+        WebElement fieldTwo = driver.findElement(By.xpath("xpath Two"));
+        // само DragAndDrop
+        Actions action = new Actions(driver);
+        action.dragAndDrop(fieldOne, fieldTwo);
+    }
+
+    @Test
+    public void selectElement() {
+        // обращение к селекту
+        Select select = new Select(driver.findElement(By.id("dropdown")));
+        // поиск по значению
+        select.selectByValue("1");
+        // поиск по индексу
+        select.selectByIndex(2);
+        // поиск по надписи
+        select.deselectByVisibleText("options 1");
+        // получение значения
+        WebElement options = select.getFirstSelectedOption();
+        // test
+        assertEquals("Options 1", options.getText());
+    }
+
+    @Test
+    public void selectWindow() {
+        // сначала открываем второе окно
+        // например жмен на кнопку
+        
+        // затем ждем открытия второго окна
+        waitForSecondWindow();
+        // считываем значения окон в коллекцию
+        Set<String> windows = driver.getWindowHandles();
+        // присваиваем переменным значения первого и второго окна
+        Iterator<String> itr = windows.iterator();
+        String parentWindow = itr.next();
+        String childWindow = itr.next();
+        // переключаемся на нужное окно
+        driver.switchTo().window(childWindow);
+        // проверяем соответствие заголовка окна
+        assertEquals(driver.getTitle(), "название окна");
+    }
+    //  ожидание открытия нового окна
+    public void waitForSecondWindow() {
+        wait.until(((ExpectedCondition<Boolean>) d -> driver.getWindowHandles().size() > 1));
+    }
+*/
 }
